c5d2c84095c01d6b6baf9191007a99cd78e656e4
Tory Taggart <tltaggar@mtu.edu>
2015-09-27 16:42:43 -0400

    Clickable nodes
    Nodes know when they are clicked on and display a number, work in
    progress

---
+++ Assets/Scripts/NodeScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NodeScript : MonoBehaviour {
+    int army;
+    Text armyvalue;
+	// Use this for initialization
+	void Start () {
+        army =100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log(hit.transform.gameObject.name);
+            }
+        }
+	
+	}
+    void OnMouseDown()
+    {
+
+    }
+    void OnGUI()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+
+        GUI.Label(new Rect(x, y, Screen.width, Screen.height), army.ToString());
+    }
+}
